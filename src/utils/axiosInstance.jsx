e33f3d9138181aeb8755df54b43d38a6814fbff7
Nilesh Vincent <60164963+Nilesh-Vincent@users.noreply.github.com>
2023-07-21 18:04:28 +0530

    Update axiosInstance.jsx

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://nilesh-uwl-final.onrender.com/api/v1",
 });
 
 axiosInstance.interceptors.request.use(
